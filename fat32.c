daa4979663be115bde46a9470d20a22123472e60
Elmer <ejr9110@LAPTOP-01920IJN.localdomain>
2020-10-01 23:08:38 -0500

    updated program

--- fat32.c
+++ fat32.c
@@ -19,6 +19,8 @@
 #define MAX_COMMAND_SIZE 255
 #define MAX_NUM_ARGUMENTS 10
 
+//Initialization of all variables needed
+//for fat32 file information and file handling
 int i;
 int16_t BPB_BytesPerSec;
 int8_t BPB_SecPerClus;
@@ -35,50 +37,58 @@ int32_t FirstDataSector = 0;
 int32_t FirstSectorofCluster = 0;
 
 
-	FILE *ofp;
-	FILE *fp; 
+FILE *ofp;
+FILE *fp; 
 
+//Function that stores the information of 
+//the fat32 file system by seeking to the bytes
+//where the information  is stored and then assigned
+//to variables so they can be displayed when "info" is
+//called
 void Fat32Info()
 {
-	    //BytesPerSec
-		fseek(fp,11,SEEK_SET);
-		fread(&BPB_BytesPerSec,1,2,fp);
-		//SrcPerClus
-		fseek(fp,13,SEEK_SET);
-		fread(&BPB_SecPerClus,1,1,fp);
-		//RsvdSecCnt
-		fseek(fp, 14, SEEK_SET);
-		fread(&BPB_RsvdSecCnt,1,2,fp);
-		//BPB_NumFATS
-		fseek(fp, 16, SEEK_SET);
-		fread(&BPB_NumFATS,1,1,fp);
-		//BPB_FATSz32
-		fseek(fp, 36, SEEK_SET);
-		fread(&BPB_FATSz32,1,4,fp);
+	//BytesPerSec
+	fseek(fp,11,SEEK_SET);
+	fread(&BPB_BytesPerSec,1,2,fp);
+	//SrcPerClus
+	fseek(fp,13,SEEK_SET);
+	fread(&BPB_SecPerClus,1,1,fp);
+	//RsvdSecCnt
+	fseek(fp, 14, SEEK_SET);
+	fread(&BPB_RsvdSecCnt,1,2,fp);
+	//BPB_NumFATS
+	fseek(fp, 16, SEEK_SET);
+	fread(&BPB_NumFATS,1,1,fp);
+	//BPB_FATSz32
+	fseek(fp, 36, SEEK_SET);
+	fread(&BPB_FATSz32,1,4,fp);
 			
-	
 }
+
+//Function used to compare a filename given by the user
+//to the filenames in the fat32.
 void expand(char *filename, char *expanded)
 {
-	
 	char expanded_name[12];
 	memset( expanded_name, ' ', 12 );
 	
 	char *token2 = strtok( filename, "." );
 	strncpy( expanded_name, token2, strlen( token2 ) );
 	token2 = strtok( NULL, "." );
-		if( token2 )
-		{
-			strncpy( (char*)(expanded_name+8), token2, strlen(token2 ) );
-		}
-		int i;
-		for( i = 0; i < 11; i++ )
-		{
-			expanded_name[i] = toupper( expanded_name[i] );
-		}  
-		strncpy(expanded,expanded_name,strlen(expanded_name));
+	if( token2 )
+	{
+		strncpy( (char*)(expanded_name+8), token2, strlen(token2 ) );
+	}
+	int i;
+	for( i = 0; i < 11; i++ )
+	{
+		expanded_name[i] = toupper( expanded_name[i] );
+	}  
+	strncpy(expanded,expanded_name,strlen(expanded_name));
 }
 
+//Function used to find the next logical block address in the first FAT 
+//of the logical block given by the user
 int16_t NextLB(uint32_t sector)
 {
 	uint32_t FATAddress = (BPB_BytesPerSec *BPB_RsvdSecCnt)+(sector * 4);
@@ -88,44 +98,18 @@ int16_t NextLB(uint32_t sector)
 	return val;
 }
 
+//Function that takes in a sector number that points to a block of data
+//and returns the starting address for the given block of data
 int LBAToOffset(int32_t sector)
 {
 	return ((sector - 2)*BPB_BytesPerSec) + (BPB_BytesPerSec * BPB_RsvdSecCnt) + (BPB_NumFATS * BPB_FATSz32*BPB_BytesPerSec);
 }
-/*void get(char *filename)
-{
-	int found = 0;
-	int i = 0;
-	
-	for(i = 0; i <11; i++)
-	{
-		if(compare(i, filename))
-		{
-			found = i;
-			break;
-		}
-	}
-	if(found == 0)
-	{
-		printf("File not found\n");
-	}
-	
-	int cluster = dir[i].FirstClusterLow);
-	int size = dir[i].FileSize;
-	int offset = LBAToOffset(cluster)fseek(fp, offset,SEEK_SET);
-	ofp.fopen(filename,"r);
-	char buff[512];
-	fread(&buff[0],512,',fp);
-	fwrite(&buff[0],512,ofp);
-	size = size-512;
-	
-	strcpy(string, dir[i].DIR_Name);
-	string[11] = '\0';
-}
-*/
 
 
 
+//Structure initialization that will store the several
+//variables associated with the files inside the fat32
+//such as their size, cluster number, name,etc.
 struct __attribute__((__packed__)) DirectoryEntry{
 char DIR_Name[11];
 uint8_t DIR_Attr;
@@ -138,103 +122,123 @@ uint32_t DIR_FileSize;
 struct DirectoryEntry dir[16];
 
 
-//int stat(const char *path, struct stat *___attribute__);
 
 int main(void)
 {
 	char *cmd_str = (char*)malloc(MAX_COMMAND_SIZE);
-	//sig_int();
+	
 	 
 	while(1)
 	{
-		printf("msf> "); 
+        //print out the shell command prompt
+		printf("mfs> "); 
+
+        // Read the command from the commandline.  The
+        // maximum command that will be read is MAX_COMMAND_SIZE
+        // This while command will wait here until the user
+        // inputs something since fgets returns NULL when there
+        // is no input
 		while(!fgets(cmd_str, MAX_COMMAND_SIZE, stdin) );
 		
 
-	char *token[MAX_NUM_ARGUMENTS];
-	int token_count = 0;
-	char *arg_ptr;
-	char *working_str = strdup(cmd_str);
-	char *working_root = working_str;
+	    char *token[MAX_NUM_ARGUMENTS];
+	    int token_count = 0;
+	    char *arg_ptr;
+	    char *working_str = strdup(cmd_str);
+	    char *working_root = working_str;
 	  
-			while( ( ( arg_ptr = strsep(&working_str, WHITESPACE) ) != NULL) && (token_count < MAX_NUM_ARGUMENTS) )
+        // Tokenize the input stringswith whitespace used as the delimiter
+		while( ( ( arg_ptr = strsep(&working_str, WHITESPACE) ) != NULL) && (token_count < MAX_NUM_ARGUMENTS) )
+		{
+			token[token_count] = strndup(arg_ptr, MAX_COMMAND_SIZE);
+		 
+			if(strlen(token[token_count]) == 0)
 			{
-				token[token_count] = strndup(arg_ptr, MAX_COMMAND_SIZE);
-			 
-				if(strlen(token[token_count]) == 0)
-				{
-					token[token_count] == NULL;
-				}
-			 token_count++;
-		
+				token[token_count] == NULL;
 			}
-	int fileOpen;
+		     token_count++;
+		
+		}
 
-			if(strcmp(token[0], "open") == 0)
+	    int fileOpen;
+
+        //Check if the "open" command was given in order
+        //to open the fat32.img file system and initialize
+        //the root directory address so we can seek to it and
+        //populate our directory array with the directories in the 
+        //FAT
+		if(strcmp(token[0], "open") == 0)
+		{
+			if (fp == NULL)
 			{
-				if (fp == NULL)
-				{
 				fp = fopen(token[1],"r");
 
-					if( fp == NULL) 
-					{
-						printf("Error: File system image not found.\n");
-					}
+				if( fp == NULL) 
+				{
+					printf("Error: File system image not found.\n");
 				}
-				else if (fp != NULL)
-				{
-					printf( "Error: File system image already open.\n"); 
-				}	
-				 Fat32Info();
-	  // Calculating address of root directory
-	  RootAddress = (BPB_NumFATS * BPB_FATSz32 * BPB_BytesPerSec) + (BPB_RsvdSecCnt * BPB_BytesPerSec);
-	  fseek(fp, RootAddress, SEEK_SET);
-	  
-	  int i;
-	  // Reads through the 16 directory entries
-	  for(i = 0; i < 16; i++)
-	  {
-		fread(&dir[i], sizeof(struct DirectoryEntry), 1, fp);
-	  }
 			}
-			if(strcmp(token[0],"close") == 0)
+			else if (fp != NULL)
 			{
-				if (fp != NULL)
-                {
-					fclose(fp);
-				}
-				else
-				{
-					printf("Error: File system not open\n");
-                }
-				fp = NULL;
-			}
-			
-				
-			if(strcmp(token[0],"info") == 0 )
-			{
-				if(fp != NULL)
-				{
-				
+				printf( "Error: File system image already open.\n"); 
+			}	
+			Fat32Info();
 
-				printf("BPB_BytesPerSec: %d %x ", BPB_BytesPerSec, BPB_BytesPerSec);
-				printf("\n");
-				printf("BPB_SecPerClus: %d %x",BPB_SecPerClus, BPB_SecPerClus);
-				printf("\n");
-				printf("BPB_RsvdSecCnt: %d %x ", BPB_RsvdSecCnt, BPB_RsvdSecCnt); 
-				printf("\n");
-				printf("BPB_NumFATS: %d %x ", BPB_NumFATS, BPB_NumFATS);
-				printf("\n");
-				printf("BPB_FATSz32: %d %x ", BPB_FATSz32, BPB_FATSz32);
-				printf("\n");
+	        // Calculating address of root directory
+	        RootAddress = (BPB_NumFATS * BPB_FATSz32 * BPB_BytesPerSec) + (BPB_RsvdSecCnt * BPB_BytesPerSec);
+	        fseek(fp, RootAddress, SEEK_SET);
+	  
+	        int i;
+	        // Reads through the 16 directory entries
+	        for(i = 0; i < 16; i++)
+	        {
+		        fread(&dir[i], sizeof(struct DirectoryEntry), 1, fp);
+	        }
+		}
+
+        //Check to see if the "Close" command was given
+        //so we can close the fat32.img in our shell
+		if(strcmp(token[0],"close") == 0)
+		{
+			if (fp != NULL)
+            {
+				fclose(fp);
 			}
+			else
+			{
+				printf("Error: File system not open\n");
+            }
+			fp = NULL;
+		}
+			
+		//Check to see if the "info" command was given
+        //and then display the fat32.img specs in both decimal
+        //and hexadecimal
+		if(strcmp(token[0],"info") == 0 )
+		{
+			if(fp != NULL)
+			{
+			    printf("BPB_BytesPerSec: %d %x ", BPB_BytesPerSec, BPB_BytesPerSec);
+			    printf("\n");
+			    printf("BPB_SecPerClus: %d %x",BPB_SecPerClus, BPB_SecPerClus);
+			    printf("\n");
+			    printf("BPB_RsvdSecCnt: %d %x ", BPB_RsvdSecCnt, BPB_RsvdSecCnt); 
+			    printf("\n");
+			    printf("BPB_NumFATS: %d %x ", BPB_NumFATS, BPB_NumFATS);
+			    printf("\n");
+			    printf("BPB_FATSz32: %d %x ", BPB_FATSz32, BPB_FATSz32);
+			    printf("\n");
+		    }
 			if(fp == NULL)
 			{
-				printf("Error: File system must be opened first.\n");
-			}
-			}
+			    printf("Error: File system must be opened first.\n");
+		    }
+		}
 			
-			
+		//Check to see if the "stat" command was given
+        //and then take the specified filename specified by the user
+        //,verify it using the expand function, and then print its
+        //attribute, filesize, and cluster number.
 		if(strcmp(token[0],"stat") == 0)
 		{	if(fp != NULL)
 			{
@@ -242,27 +246,25 @@ int main(void)
 			char expanded_name[12]; 
 			expand(token[1], &expanded_name[0]);
 			expanded_name[11] = '\0';
-
-				for(i = 0; i < 16; i++)
-				{
-					int attr;
-					int first_char;
-					char name[12];
-					memcpy(name, dir[i].DIR_Name,12);
-					name[11] = '\0';
-					attr = dir[i].DIR_Attr;
-					first_char = dir[i].DIR_Name[0];
+            for(i = 0; i < 16; i++)
+			{
+				int attr;
+				int first_char;
+				char name[12];
+				memcpy(name, dir[i].DIR_Name,12);
+				name[11] = '\0';
+				attr = dir[i].DIR_Attr;
+				first_char = dir[i].DIR_Name[0];
 					
-					if(((attr == 0x01)||(attr == 0x10)||(attr == 0x20))&&((first_char != 0x00) && (first_char!= 0xE5)))
-						{
-							if(strcmp(name, expanded_name) == 0)
-							{
-							printf("Attribute \t Size \t Starting Cluster Number \n");
-							printf("%d \t\t %d \t\t %d \n",dir[i].DIR_Attr,dir[i].DIR_FileSize,dir[i].DIR_FirstClusterLow);
-							}
-						}
+				if(((attr == 0x01)||(attr == 0x10)||(attr == 0x20))&&((first_char != 0x00) && (first_char!= 0xE5)))
+				{
+					if(strcmp(name, expanded_name) == 0)
+					{
+					    printf("Attribute \t Size \t Starting Cluster Number \n");
+						printf("%d \t\t %d \t\t %d \n",dir[i].DIR_Attr,dir[i].DIR_FileSize,dir[i].DIR_FirstClusterLow);
+					}     
 				}
-				
+			}
 							
 			}
 			if(fp == NULL)
@@ -271,6 +273,10 @@ int main(void)
 			}
 		}
 		
+        //Check to see if the "get" command was given
+        //and then take the specified file and move it into
+        // the current working directory of the user.
+        //*****NOT IMPLEMENTED CURRENTLY
 		if(strcmp(token[0],"get") == 0)
 		{
 			if(fp != NULL)
@@ -282,26 +288,76 @@ int main(void)
 			}
 		}
 		
-		if(strcmp(token[0],"put") == 0)
-		{
-			if(fp != NULL)
-			{
-			}
-			if(fp == NULL)
-			{
-				printf("Error: File system must be opened first.\n");
-			}
-		}
+        //Check to see if the "cd" command was given
+        //and establish the path to the directory the user
+        //specified in order to make it the current working 
+        //directory
 		if(strcmp(token[0],"cd") == 0)
 		{
 			if(fp != NULL)
 			{
+                int addresses[300];
+                char expanded_name[12]; 
+                int count=0;
+                if(strcmp(token[1],"..")==0)
+                {
+                    int parentoffs;
+                    
+                    int cluster=dir[1].DIR_FirstClusterLow;
+                    if(cluster==0)
+                    {
+                        parentoffs=RootAddress;
+                        fseek(fp,parentoffs,SEEK_SET);
+                        fread(&dir[0],16, sizeof(struct DirectoryEntry), fp);
+                    }
+                    else
+                    {
+                        parentoffs=LBAToOffset(cluster);
+                        fseek(fp,parentoffs,SEEK_SET);
+                        fread(&dir[0],16, sizeof(struct DirectoryEntry), fp);
+                    }
+                    
+                    
+                }
+                else
+                {
+				    for(i = 0; i < 16; i++)
+				    {
+                        int attr;
+					    int first_char;
+					    char name[12];
+                        int offs;
+                        expand(token[1], &expanded_name[0]);
+			            expanded_name[11] = '\0';
+					    memcpy(name, dir[i].DIR_Name,12);
+					    name[11] = '\0';
+					    attr = dir[i].DIR_Attr;
+					    first_char = dir[i].DIR_Name[0];
+                    
+					    if(((attr == 0x01)||(attr == 0x10)||(attr == 0x20))&&((first_char != 0x00) && (first_char!= 0xE5)))
+					    {
+						    if(strcmp(name, expanded_name) == 0)
+						    {
+                                offs=LBAToOffset(dir[i].DIR_FirstClusterLow);
+                                addresses[count]=offs;
+                                count++;
+                                fseek(fp,offs,SEEK_SET);
+                                fread(&dir[0],16, sizeof(struct DirectoryEntry), fp);
+						    }
+					    }
+				    }
+                }
 			}
 			if(fp == NULL)
 			{
 				printf("Error: File system must be opened first.\n");
 			}
 		}
+        
+        //Check to see if the "ls" command was given
+        //in order to display all the files/directories in the current directory
+        //the user is in. Only files and directories that have not been deleted
+        //and are read only, and are subdirectories are shown
 		if(strcmp(token[0],"ls") == 0)
 		{
 			if(fp != NULL)
@@ -311,12 +367,10 @@ int main(void)
 				char name[12];
 				for(i = 0; i < 16; i++)
 				{
-					
 					strncpy(name, dir[i].DIR_Name,12);
 					name[11] = '\0';
 					attr = dir[i].DIR_Attr;
 					first_char = dir[i].DIR_Name[0];
-					
 					if(((attr == 0x01)||(attr == 0x10)||(attr == 0x20))&&((first_char != 0x00) && (first_char!= 0xE5)))
 						{
 							
@@ -330,6 +384,10 @@ int main(void)
 				printf("Error: File system must be opened first.\n");
 			}
 		}
+
+        //Check to see if the "read" command was given
+        //and reads the user given file at the position, in bytes, specified by 
+        //the position parameter and outputs the number of bytes specified. 
 		if(strcmp(token[0],"read") == 0)
 		{
 			if(fp != NULL)
@@ -389,7 +447,10 @@ int main(void)
 					}
 				}
 			}
-					
+
+            //Check that executes whenever the user attempts to 
+            //enter commands and the fat32.img file has not been
+            //opened yet.	
 			if(fp == NULL)
 			{
 				printf("Error: File system must be opened first.\n");
@@ -399,4 +460,5 @@ int main(void)
 	
 	
  return EXIT_SUCCESS;
- 
+
+}
